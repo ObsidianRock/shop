a7e0e8eb518204e4f16f1c4b536ef93e1c5090f9
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-11 17:00:03 +0100

    celery task for orders email

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+
+from cart.cart import Cart
 from .models import OrderItem
 from .forms import OrderCreateForm
-from cart.cart import Cart
+
+from .tasks import order_created
 
 
 def order_create(request):
@@ -18,6 +21,7 @@ def order_create(request):
                                          quantity=item['quantity'])
 
                 cart.clear()
+                order_created.delay(order.id)
                 return render(request,
                               'orders/order/created.html',
                               {'order': order})
