0a2cf83b994e5a8baa1f508b43ea3d90d8455aa0
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-12 16:44:50 +0100

    adding coupon to cart

--- cart/cart.py
+++ cart/cart.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 from django.conf import settings
 from myshop.models import Product
 
+from coupons.models import Coupon
+
 
 class Cart:
 
@@ -15,6 +17,7 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, update_quantity=False):
 
@@ -68,3 +71,17 @@ class Cart:
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
         self.session.modified = True
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100')) * self.get_total_price()
+        return Decimal('0')
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,8 @@ from myshop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
+from coupons.forms import CouponApplyForm
+
 
 @require_POST
 def cart_add(request, product_id):
@@ -31,9 +33,14 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
-    
+
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(
         initial={'quantity': item['quantity'],'update': True})
 
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+
+    return render(request,
+                  'cart/detail.html',
+                  {'cart': cart,
+                   'coupon_apply_form': coupon_apply_form})
