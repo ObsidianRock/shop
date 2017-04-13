63582a96b99169efe30bafb2a74ae5c09bbe4dbf
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-13 14:24:23 +0100

    Translating model fields

--- myshop/models.py
+++ myshop/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from parler.models import TranslatableModel, TranslatedFields
 
 
-class Category(models.Model):
+class Category(TranslatableModel):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    translations = TranslatedFields(
+
+        name=models.CharField(max_length=200,
+                              db_index=True),
+        slug=models.SlugField(max_length=200,
+                              db_index=True,
+                              unique=True)
+    )
 
     def get_absolute_url(self):
         return reverse('myshop:product_list_by_category', args=[self.slug])
 
     class Meta:
-        ordering = ('name',)
+        #ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -18,13 +28,19 @@ class Category(models.Model):
         return self.name
 
 
-class Product(models.Model):
+class Product(TranslatableModel):
     category = models.ForeignKey(Category, related_name='products')
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-
     description = models.TextField(blank=True)
+
+    translations = TranslatedFields(
+        name=models.CharField(max_length=200, db_index=True),
+        slug=models.SlugField(max_length=200, db_index=True),
+        description=models.TextField(blank=True)
+    )
+
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
@@ -36,7 +52,7 @@ class Product(models.Model):
 
     class Meta:
         ordering = ('name',)
-        index_together = (('id', 'slug'),)
+        #index_together = (('id', 'slug'),)
 
     def __str__(self):
         return self.name
